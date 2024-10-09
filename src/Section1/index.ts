a6a99e8311314b1b9315e86429b5f3070e563a0d
a.chiba <a.chiba@uchida-it.co.jp>
2024-10-09 17:44:00 +0900

    ランタイムの挙動は型情報に依存しない

---
+++ src/Section1/index.ts
@@ -0,0 +1,26 @@
+// const str: string = "foobar";
+
+function repeatHello(count: number): string {
+    return "hello".repeat(count);
+}
+
+console.log(repeatHello(10));
+
+// ダメな例
+// function double(value: number) {
+//     console.log(value * 2);
+// }
+// function double(value: string) {
+//     console.log(value.repeat(2));
+// }
+
+function double(value: string | number) {
+    if (typeof value === "number") {
+        console.log(value * 2);
+    } else {
+        console.log(repeatHello(2));
+    }
+}
+
+double(223);
+double("hello");
--- src/chapter1/index.ts
+++
@@ -1,7 +0,0 @@
-// const str: string = "foobar";
-
-function repeatHello(count: number): string {
-    return "hello".repeat(count);
-}
-
-console.log(repeatHello(10));

213f3409b5b207104dfa928509d381769a9d8ee7
a.chiba <a.chiba@uchida-it.co.jp>
2024-12-01 22:15:12 +0900

    p82 オブジェクトへの参照になる

--- src/Section3/index.ts
+++ src/Section3/index.ts
@@ -1,17 +1,45 @@
+import { on } from "events";
 import { createInterface } from "readline";
 
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const rl = createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-const messages = {
-    good: "0以上の数値が入力されました!",
-    bad: "負の数値を入力しないで下さい!"
+// const messages = {
+//     good: "0以上の数値が入力されました!",
+//     bad: "負の数値を入力しないで下さい!"
+// }
+
+// rl.question('数値を入力してください:', (line) => {
+//     const num = Number(line);
+//     console.log(messages[num > 0 ? "good" : "bad"]);
+//     rl.close();
+// });
+
+// const obj1 = {
+//     foo: 123,
+//     bar: 456,
+// };
+
+// const obj2 = {
+//     bar: -999,
+//     baz: -9999,
+// }
+
+// const obj3 = {
+//     ...obj1,
+//     ...obj2,
+// }
+
+// console.log(obj3);
+
+const foo = {
+    num: 123,
 }
+const bar = foo;
+console.log(bar.num);
+bar.num = 0;
+console.log(foo.num);
 
-rl.question('数値を入力してください:', (line) => {
-    const num = Number(line);
-    console.log(messages[num > 0 ? "good" : "bad"]);
-    rl.close();
-});
+
